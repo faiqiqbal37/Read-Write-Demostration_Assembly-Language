76c555e3fb334d1908e5b371e12e0af956d2d62c
faiqiqbal37 <55352092+faiqiqbal37@users.noreply.github.com>
2021-10-23 08:54:13 -0700

    Add files via upload

---
+++ read-write_Assembly-Language_example.3.asm
@@ -0,0 +1,161 @@
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+.MODEL SMALL
+.STACK 100H
+.DATA
+
+STR DB "**********$"
+
+
+.CODE
+
+    MAIN PROC
+    
+    MOV AX, @DATA
+    MOV DS, AX
+  
+    
+    LEA DX, STR
+    MOV AH, 9
+    INT 21H
+     
+    
+
+    MOV AH, 2
+    MOV DL, 10
+    INT 21H   
+    MOV DL, 13
+    INT 21H
+    
+    
+    LEA DX, STR
+    MOV AH, 9
+    INT 21H
+     
+    
+
+    MOV AH, 2
+    MOV DL, 10
+    INT 21H   
+    MOV DL, 13
+    INT 21H
+    
+    
+    LEA DX, STR
+    MOV AH, 9
+    INT 21H
+     
+    
+
+    MOV AH, 2
+    MOV DL, 10
+    INT 21H   
+    MOV DL, 13
+    INT 21H
+    
+    
+    LEA DX, STR
+    MOV AH, 9
+    INT 21H
+     
+    
+
+    MOV AH, 2
+    MOV DL, 10
+    INT 21H   
+    MOV DL, 13
+    INT 21H
+    
+    
+    LEA DX, STR
+    MOV AH, 9
+    INT 21H
+     
+    
+
+    MOV AH, 2
+    MOV DL, 10
+    INT 21H   
+    MOV DL, 13
+    INT 21H
+    
+    
+    LEA DX, STR
+    MOV AH, 9
+    INT 21H
+     
+    
+
+    MOV AH, 2
+    MOV DL, 10
+    INT 21H   
+    MOV DL, 13
+    INT 21H
+    
+    
+    LEA DX, STR
+    MOV AH, 9
+    INT 21H
+     
+    
+
+    MOV AH, 2
+    MOV DL, 10
+    INT 21H   
+    MOV DL, 13
+    INT 21H
+    
+    
+    LEA DX, STR
+    MOV AH, 9
+    INT 21H
+     
+    
+
+    MOV AH, 2
+    MOV DL, 10
+    INT 21H   
+    MOV DL, 13
+    INT 21H
+    
+    
+    LEA DX, STR
+    MOV AH, 9
+    INT 21H
+     
+    
+
+    MOV AH, 2
+    MOV DL, 10
+    INT 21H   
+    MOV DL, 13
+    INT 21H 
+    
+    
+    
+    LEA DX, STR
+    MOV AH, 9
+    INT 21H
+     
+    
+
+    MOV AH, 2
+    MOV DL, 10
+    INT 21H   
+    MOV DL, 13
+    INT 21H
+    
+
+
+    MAIN ENDP
+    
+    
+ END MAIN
+    
+    
+
+ret
+
